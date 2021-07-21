e75c7614ba94ea75d259b56f4461bab8f94aef84
omussell <oliver.mussell@hotmail.co.uk>
2021-07-21 21:56:27 +0100

    Initial command line flags working properly

--- src/bun.nim
+++ src/bun.nim
@@ -1,1 +1,25 @@
 ## This module takes a FQDN as input and returns the contents of the matching YAML file.
+
+import parseopt
+
+proc writeHelp() = echo "get help"
+
+proc writeVersion() = echo "1.0.0"
+
+var args: OptParser = initOptParser()
+var fqdn: string
+
+for kind, key, val in args.getopt():
+    case kind
+    of cmdArgument:
+      fqdn = key
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "help", "h": writeHelp()
+      of "version", "v": writeVersion()
+    of cmdEnd: break
+
+echo fqdn
+
+
+#proc bun*(question: string): Answer =
